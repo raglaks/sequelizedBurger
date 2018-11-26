dea41bbd9ca60c38df99bbc34b7a8730210af98b
Raghava Lakshminarayana <raglaks94@gmail.com>
2018-11-26 17:38:08 -0600

    add routes files

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const db = require("./models");
+
+const app = express();
+let PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+//routes go here
+
+// const exphbs = require("express-handlebars");
+
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
+
+db.sequelize.sync({ force: true }).then(function () {
+    app.listen(PORT, function () {
+        console.log("App listening on PORT " + PORT);
+    });
+});
